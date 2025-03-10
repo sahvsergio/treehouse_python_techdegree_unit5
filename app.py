3a6fe33e61e8149eeda4b61fccb460c821a60e17
sahvsergio <sergioandresherrera@gmail.com>
2025-03-09 21:42:37 -0500

    recover the header

--- app.py
+++ app.py
@@ -191,4 +191,4 @@ if __name__ == '__main__':
     # making the app run, you just need to run the app.py file on the terminal
     # local app.run(debug=True , port=8000, host='127.0.0.1')
     # internet
-    app.run( port=8000,debug=True, host='127.0.0.1')
+    app.run( port=8000,debug=False, host='127.0.0.1')
