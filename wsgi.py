cde0fae7b5bc81d4d05b56f3b0a3c1fcbcb75c58
sahvsergio <sergioandresherrera@gmail.com>
2025-03-16 22:48:18 -0500

    prepare for deploy

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,7 @@
-from models import app, db
+from app import app, db
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
-        # creating the engine
         engine = db.engine
         db.create_all()
-
-    # making the app run, you just need to run the app.py file on the terminal
-    # local app.run(debug=True , port=8000, host='127.0.0.1')
-     # internet
-    app.run( port=8000, debug=True, host='127.0.0.1')
+    app.run()
