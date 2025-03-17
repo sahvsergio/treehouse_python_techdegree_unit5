9101e3d0f9410f969cf0216fc6cb2e2c7c4fb123
sahvsergio <sergioandresherrera@gmail.com>
2025-03-16 23:11:45 -0500

    add applicationt to wsgi

--- app.py
+++ app.py
@@ -155,13 +155,13 @@ def not_found(error):
     return render_template('404.html', projects=projects), 404
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        # creating the engine
-        engine = db.engine
-        db.create_all()
+#if __name__ == '__main__':
+#    with app.app_context():
+#        # creating the engine
+#        engine = db.engine
+#        db.create_all()
 
     # making the app run, you just need to run the app.py file on the terminal
     # local app.run(debug=True , port=8000, host='127.0.0.1')
      #internet
-    app.run( port=8000,debug=False, host='0.0.0.0')
+#    app.run( port=8000,debug=False, host='0.0.0.0')
---
+++ wsgi.py
@@ -0,0 +1,17 @@
+from app import app
+
+# WSGI requires an application callable
+application = app
+
+
+
+if __name__ == '__main__':
+    with application.app_context():
+        # creating the engine
+        engine = db.engine
+        db.create_all()
+
+    # making the app run, you just need to run the app.py file on the terminal
+    # local app.run(debug=True , port=8000, host='127.0.0.1')
+     # internet
+    application.run( port=8000, debug=False, host='0.0.0.0')
