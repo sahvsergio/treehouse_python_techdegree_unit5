ead973622675a5940838e478549d56d3c4510756
sahvsergio <sergioandresherrera@gmail.com>
2025-03-18 20:49:24 -0500

    merged local branch backend into main

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 # flask imports
 from flask import flash, redirect, render_template, request, url_for, flash, jsonify
 from models import Project, Flask, app, db
+from flask_basicauth import BasicAuth
 import datetime
 
 
@@ -28,23 +29,15 @@ from flask_admin import Admin
 
 from flask_admin import form
 
-from flask_bootstrap import Bootstrap
 
 from flask_admin.contrib.sqla import ModelView
 
 
+app.config['BASIC_AUTH_USERNAME'] = 'john'
+app.config['BASIC_AUTH_PASSWORD'] = 'matrix'
+basic_auth = BasicAuth(app)
+
 # create routes(visible parts of the site- urls)
-
-# admin view
-##
-
-admin = Admin(app, name='Dashboard')
-
-
-
-
-
-
 
 @app.route('/', methods=['GET', 'POST'])
 # decorator
@@ -61,6 +54,7 @@ def index():
 
 
 @app.route('/projects/new', methods=['GET', 'POST'])
+@basic_auth.required
 def create():
     '''Turns the string into a date object'''
     # 'title', 'Hello'),
@@ -103,6 +97,7 @@ def detail(id):
 
 
 @app.route('/projects/<id>/edit', methods=['GET', 'POST'])
+@basic_auth.required
 def edit(id):
 
     projects = db.session.query(Project).all()
@@ -134,6 +129,7 @@ def edit(id):
 
 
 @app.route('/projects/<int:id>/delete')
+@basic_auth.required
 def delete(id):
     projects = db.session.query(Project).all()
     project = db.one_or_404(db.select(Project).filter_by(id=id))
@@ -155,13 +151,12 @@ def not_found(error):
     return render_template('404.html', projects=projects), 404
 
 
-#if __name__ == '__main__':
-#    with app.app_context():
+if __name__ == '__main__':
+  with app.app_context():
 #        # creating the engine
-#        engine = db.engine
-#        db.create_all()
-
-    # making the app run, you just need to run the app.py file on the terminal
-    # local app.run(debug=True , port=8000, host='127.0.0.1')
+        engine = db.engine
+        db.create_all()
+        # making the app run, you just need to run the app.py file on the terminal
+        # local app.run(debug=True , port=8000, host='127.0.0.1')
      #internet
-#    app.run( port=8000,debug=False, host='0.0.0.0')
+  app.run( port=8000,debug=True, host='0.0.0.0')
--- wsgi.py
+++ wsgi.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
     # making the app run, you just need to run the app.py file on the terminal
     # local app.run(debug=True , port=8000, host='127.0.0.1')
      # internet
-    application.run( port=8000, debug=False, host='0.0.0.0')
+    application.run( port=8000, debug=True, host='0.0.0.0')
