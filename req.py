35ac480d827b665bfb29787e145fb87fee8fedb3
sahvsergio <sergioandresherrera@gmail.com>
2025-07-14 23:51:00 -0500

    completed project 10 for javascript techdegree

---
+++ req.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+
+my_info = requests.get('https://teamtreehouse.com/sahvsergio.json')
+
+
+#with open('treehouse.json','w') as treehouse_info:
+#    treehouse_info.write(str(my_info.text))
+   
+    
+
