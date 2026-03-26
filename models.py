aa74d49fa39ebb1298252fb517b96d3579ed244d
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2026-03-26 19:47:14 +0000

    Update models.py

--- models.py
+++ models.py
@@ -24,7 +24,7 @@ app = Flask(__name__, instance_path=f'{os.getcwd()}', template_folder='templates
 
 # 🔹 Load environment variables
 DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = (os.getenv('DB_PASSWORD'))
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_NAME = os.getenv('DB_NAME')
 
