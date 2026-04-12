0266ea2dcf87bba1af131fa53caa208e7c27de89
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2026-04-12 22:43:40 +0000

    Update req.py

--- req.py
+++ req.py
@@ -1,16 +1,20 @@
 import requests
 import json
-import os 
+import os
+import tracebback
 
 TREEHOUSE_DATA= os.getenv('TREEHOUSE_DATA')
-
-my_info = requests.get(TREEHOUSE_DATA)
-
-
+try:
+    my_info = requests.get(TREEHOUSE_DATA)
+except Exception:
+    print("""---ERROR DETECTED---""")
+    traceback,print_exc()
+    
 with open('treehouse.json','w') as treehouse_info:
     treehouse_info.write(str(my_info.text))
+    
 
-pwith
+
    
     
 
