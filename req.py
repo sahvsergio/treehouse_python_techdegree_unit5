8c0fc1b7c00c12fc9df5a26e4008a1d2adab5e18
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2026-04-12 21:39:20 +0000

    Read treehouse data Update req.py

--- req.py
+++ req.py
@@ -1,12 +1,16 @@
 import requests
 import json
+import os 
+
+TREEHOUSE_DATA= os.getenv('TREEHOUSE_DATA')
+
+my_info = requests.get(TREEHOUSE_DATA)
 
 
-my_info = requests.get('https://teamtreehouse.com/sahvsergio.json')
+with open('treehouse.json','w') as treehouse_info:
+    treehouse_info.write(str(my_info.text))
 
-
-#with open('treehouse.json','w') as treehouse_info:
-#    treehouse_info.write(str(my_info.text))
+pwith
    
     
 
